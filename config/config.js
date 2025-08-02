91debd5ab7516865bb31091e65fe2720d7732a5d
eagleminale <minale.fetene@eaglelionsystems.com>
2025-08-02 14:38:12 +0300

    change DB to leapcell

--- config/config.js
+++ config/config.js
@@ -3,25 +3,27 @@ require('dotenv').config(); // Ensure dotenv is loaded here
 
 module.exports = {
   development: {
-    use_env_variable: 'postgresql://neondb_owner:npg_fsU07WANQuiz@ep-red-poetry-a2ejalxz-pooler.eu-central-1.aws.neon.tech/aahdc_Prod?sslmode=require&channel_binding=require', // Use the DATABASE_URL from .env
-    dialect: 'postgres',
+    use_env_variable:
+      "postgresql://ysowiltmwclkinstkeef:kucxxxviqdvxkiybmkyczdjkjtmnsl@9qasp5v56q8ckkf5dc.leapcellpool.com:6438/goztuovzifaupnqcgvcb?sslmode=require", // Use the DATABASE_URL from .env
+    dialect: "postgres",
     dialectOptions: {
       ssl: {
         require: true, // Enforce SSL connection
-        rejectUnauthorized: false // Set to true in production with valid certs, false for self-signed or development ease
-      }
+        rejectUnauthorized: false, // Set to true in production with valid certs, false for self-signed or development ease
+      },
     },
     logging: false, // Set to true to see SQL queries in console
   },
   production: {
-    use_env_variable: 'postgresql://neondb_owner:npg_fsU07WANQuiz@ep-weathered-star-a2p5as5z-pooler.eu-central-1.aws.neon.tech/aahdc-lottery-db?sslmode=require&channel_binding=require' ,
-    dialect: 'postgres',
+    use_env_variable:
+      "postgresql://ysowiltmwclkinstkeef:kucxxxviqdvxkiybmkyczdjkjtmnsl@9qasp5v56q8ckkf5dc.leapcellpool.com:6438/goztuovzifaupnqcgvcb?sslmode=require",
+    dialect: "postgres",
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: true // Always true in production
-      }
+        rejectUnauthorized: true, // Always true in production
+      },
     },
     logging: false,
-  }
+  },
 };
