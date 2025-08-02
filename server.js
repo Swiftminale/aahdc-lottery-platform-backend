31907f30abd9942a92fed1c109d10fa9ef9df411
eagleminale <minale.fetene@eaglelionsystems.com>
2025-08-02 16:02:48 +0300

    cors

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(express.json());
 // =========================================================================
 // API Routes
--- src/routes/unitRoutes.js
+++ src/routes/unitRoutes.js
@@ -1,7 +1,8 @@
 // backend/src/routes/unitRoutes.js
 import { Router } from 'express';
 const router = Router();
-import { submitUnits, getUnits } from '../controllers/unitController';
+// import { submitUnits, getUnits } from '../controllers/unitController';
+import { submitUnits, getUnits } from "../controllers/unitController.js";
 
 router.post('/', submitUnits);
 router.get('/', getUnits);
